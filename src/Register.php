bd69d2096df85d5651961ff11b0d0224979edc92
Burrito97 <Account@Silvester>
2020-05-21 23:42:03 +0200

    E bela ne php

---
+++ src/Register.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<title>Register</title>
+	</head>
+	<body>
+		<h2>Registration Page</h2>
+		<a href="index1.php">Click here to go back</a><br/><br/>
+		<form action="register1.php" method="post">
+			Enter Name: <input type="text" name="name" required="required"/> <br/>
+			Enter Email: <input type="text" name="email" required="required" /> <br/>
+			<input type="submit" value="Register"/>
+		</form>
+	</body>
+</html>
+
+<?php
+ $db = mysqli_connect('localhost', 'root', '', 'register1');
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+	$name= mysqli_real_escape_string($db,$_POST['name']);
+	$email= mysqli_real_escape_string($db,$_POST['email']);
+	$bool = true;
+	mysqli_connect("localhost", "root","") or die(mysqli_error()); 
+	mysqli_select_db($db,"register1") or die("Cannot connect to database"); 
+	$query = mysqli_query($db,"Select * from users"); 
+	while($row = mysqli_fetch_array($query)) 
+	{
+		$table_users = $row['name'];
+		if($name == $table_users) 
+		{
+			$bool = true; 
+			Print '<script>window.location.assign("register1.php");</script>';
+		}
+	}
+	if($bool) 
+	{
+		mysqli_query($db,"INSERT INTO users (name, email) VALUES ('$name','$email')"); 
+		Print '<script>alert("Successfully Registered!");</script>'; 
+		Print '<script>window.location.assign("register1.php");</script>'; 
+	}
+}
+?>
---
+++ src/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Register</title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello There!</p>";
+        ?>
+		
+       <a href="register1.php"> Click here to register <a/> <br/> 
+		
+    </body>
+</html>
